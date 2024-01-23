a03b6a109cb54d87cb2a664d6e8bfe35eaecb894
Bergenisto <153793945+Bergenisto@users.noreply.github.com>
2024-01-23 18:21:38 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -7,7 +7,7 @@ person[0] + " " + person.length;
 
 const person2 = [];
 person[0] = "Joh";
-person[1] = "Doe";
+person[1] = "De";
 person[2] = 43; 
 document.getElementById("deo").innerHTML =
 person[0] + " " + person.lengt;
