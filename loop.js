09e783fcb3f2632297a2b68ec9c42e330e98f038
Bergenisto <153793945+Bergenisto@users.noreply.github.com>
2024-02-12 15:44:53 +0100

    Update loop.js

--- loop.js
+++ loop.js
@@ -2,7 +2,7 @@ const person = [];
 person[0] = "John";
 person[1] = "Do";
 person[2] = 83; 
-document.getlemetyd("demo").innrML =
+document.getlemetyd("demo").innML =
 person[0] + "  " + person.length;
 
 const person2 = [];
